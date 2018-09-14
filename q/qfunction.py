84e16ae71ffb376913c9a4ccfa97195501cc25dc
Jan Janech <janik@janik.ws>
2018-09-14 14:52:12 +0200

    named Q constructors - empty, count, range, repeat

--- q/qfunction.py
+++ q/qfunction.py
@@ -1,4 +1,5 @@
-from typing import Union, TypeVar, Iterable, Iterator, Generic
+from itertools import repeat, count
+from typing import Union, TypeVar, Iterable, Iterator, Generic, Optional, Any
 
 from .iterablehelper import IterableHelper
 from .query import Query
@@ -22,9 +23,36 @@ class QFunction:
         else:
             raise TypeError('collection parameter of Q() function has to be Queryable, Iterable, or Iterator')
 
+    def empty(self) -> Query[Any]:
+        return self(())
+
+    def count(self, start: int) -> Query[int]:
+        return self(count(start))
+
+    def range(self, start: int, count: int) -> Query[int]:
+        return self(range(start, start + count))
+
+    def repeat(self, element: T, count: Optional[int] = None) -> Query[T]:
+        if count is None:
+            return self(repeat(element))
+        else:
+            return self(repeat(element, count))
+
 
 class Q(metaclass=QFunction):
     def __new__(cls, collection: Union[Queryable[T], Iterable[T], Iterator[T]]) -> Query[T]: ...
+
+    @staticmethod
+    def empty() -> Query[Any]: ...
+
+    @staticmethod
+    def count(start: int) -> Query[int]: ...
+
+    @staticmethod
+    def range(start: int, count: int) -> Query[int]: ...
+
+    @staticmethod
+    def repeat(element: T, count: Optional[int] = None) -> Query[T]: ...
 
 
 __ALL__ = ['Q']
---
+++ tests/test_statics.py
@@ -0,0 +1,16 @@
+import unittest
+
+from q import Q
+
+
+class MyTestCase(unittest.TestCase):
+    def test_empty(self):
+        self.assertSequenceEqual(Q.empty().to_list(), [])
+
+    def test_range(self):
+        self.assertSequenceEqual(Q.range(1, 10).to_list(), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    def test_repeat_with_count(self):
+        self.assertSequenceEqual(Q.repeat('A', 5).to_list(), ['A', 'A', 'A', 'A', 'A'])
+
+    #TODO: test infinite Q.count and Q.repeat
