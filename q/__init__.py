5f896e592ce942a2a81b3616b7bfdbb28c00e5df
Jan Janech <janik@janik.ws>
2018-09-17 15:39:11 +0200

    simple setup.py

--- q/__init__.py
+++ q/__init__.py
@@ -1,3 +1,6 @@
 from .query import Query
 from .queryable import Queryable
 from .qfunction import Q
+
+name = "pycq"
+__version__ = "0.1-alpha"
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+import q
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=q.name,
+    version=q.__version__,
+    author="Ján Janech",
+    author_email="janik@janik.ws",
+    description="A simple software library helping with processing collections.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/janusko/pycq",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    test_suite="tests"
+)
