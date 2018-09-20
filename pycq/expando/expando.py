6aec5d9b4d691f859454fa6383348aa17b4687cb
Jan Janech <janik@janik.ws>
2018-09-20 11:39:53 +0200

    nice Expando string representation

--- pycq/expando/expando.py
+++ pycq/expando/expando.py
@@ -13,3 +13,12 @@ class Expando:
         if self.__ro:
             raise AttributeError("attribute '{0}' of 'Obj' objects is not writable")
         super().__setattr__(key, value)
+
+    def __str__(self):
+        return "Expando({0})".format(
+            ", ".join(
+                "{0}={1!r}".format(name, value) for name, value in self.__dict__.items() if not name.startswith('_')
+            )
+        )
+
+    __repr__ = __str__
